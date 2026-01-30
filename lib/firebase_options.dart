4d823ae78afd2cf9755b6a5347b5ef44b70200f1
Noiyux <150844004+Noiyux@users.noreply.github.com>
2026-01-30 23:16:50 +0800

    Change Firebase API key in firebase_options.dart
    Updated the Firebase API key for Android configuration.

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get android => const FirebaseOptions(
-    apiKey: "AIzaSyCrhASUsPCbm7RCcmd_lGJAHccjZKRARNY",
+    apiKey: ".......",
     appId: "1:655932622280:android:7d2301da4f773cf751ecfd",
     messagingSenderId: "655932622280",
     projectId: "laporfasilitas-bf2f9",
